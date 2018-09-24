6fde8defe2d9a9e48ef216eaff9046e38cb94c59
Jordi Raya Lapuente <raya.jordi@gmail.com>
2018-09-24 13:50:34 +0200

    Section 3: Node.js fundamentals - nodemon

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,36 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-// console.log(_.isString(true));
-// console.log(_.isString('Jordi'));
-var filteredArray = _.uniq(['Jordi', 1, 'Jordi', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-var user = os.userInfo();
-// console.log(user);
-
-var res = notes.addNote();
-console.log(res);
-
-console.log('Result:', notes.add(9, -2));
-
-/*
-// synch
-fs.appendFileSync('greetings.txt', 'Hello ' + user.username + '!\n');
-*/
-
-// sync with template syntax (use `)
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! you are ${notes.age}\n`);
-
-/*
-// async
-fs.appendFile('greetings.txt', 'Hello world!\n', function(err) {
-	if (err) {
-		console.log('unable to write a file');
-	}
-});
-*/
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,14 +1,6 @@
 console.log('Starting notes.js');
 
-// console.log(module);
-
-module.exports.age = 45;
-
 module.exports.addNote = () => {
 	console.log('addNote');
 	return 'New note';
-}
-
-module.exports.add = (a, b) => {
-	return a + b;
 }
