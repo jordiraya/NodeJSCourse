e147150f34522eaf8e312cd921374cf0e5ba3b11
Jordi Raya Lapuente <raya.jordi@gmail.com>
2018-09-24 13:02:39 +0200

    Section 3: Node.js fundamentals - 3rd party modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,7 +2,13 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
+
+// console.log(_.isString(true));
+// console.log(_.isString('Jordi'));
+var filteredArray = _.uniq(['Jordi', 1, 'Jordi', 1, 2, 3, 4]);
+console.log(filteredArray);
 
 var user = os.userInfo();
 // console.log(user);
