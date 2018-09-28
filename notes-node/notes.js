5569f07802b97101626fbef29d3b1d6e86cd8bd9
Jordi Raya Lapuente <raya.jordi@gmail.com>
2018-09-28 15:27:20 +0200

    Section 3: Node.js fundamentals - JSON

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,6 +1,55 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New note';
+const fs = require('fs');
+
+var fetchNotes = () => {
+	try {
+		var notesString = fs.readFileSync('notes-data.json');
+		return JSON.parse(notesString);
+	} catch (e)  {
+		return [];
+	}
+};
+
+var saveNotes = (notes) => {
+	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+	var notes = fetchNotes();
+	var note = {
+		title,
+		body
+	};
+
+	// the same as -> var duplicateNotes = notes.filter((note) => {
+	// 	return note.title === title
+	// });
+	var duplicateNotes = notes.filter((note) => note.title === title);
+
+	if (duplicateNotes.length === 0) {
+		notes.push(note);
+		saveNotes(notes);
+		return note;
+	}
+};
+
+var getAll = () => {
+	console.log('Getting all notes');
+}
+
+var getNote = (title) => {
+	console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+	console.log('Removing note', title);
+};
+
+
+module.exports = {
+	addNote, // the same as addNote:addNote
+	getAll,
+	getNote,
+	removeNote
 }
