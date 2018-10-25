75f6155b923bd60a25c342fa502b28599768eef3
Jordi Raya Lapuente <raya.jordi@gmail.com>
2018-10-26 01:42:46 +0200

    Section 6.55: Testing express applications

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,9 +3,25 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Hello world!');
 });
+
+app.get('/users', (req, res) => {
+    res.send([{
+        name: 'Teodosio',
+        age: 45
+    }, {
+        name: 'Arcadio',
+        age: 43       
+    }, {
+        name: 'Honorato',
+        age: 57        
+    }]);
+});
+
 
 app.listen(3000, () => {
     console.log('Server is listening at port 3000');
 });
+
+module.exports.app = app;
---
+++ node-tests/server/server.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const expect = require('expect');
+
+var app = require('./server').app;
+
+it('should return hello world response', (done) => {
+    request(app)
+        .get('/')
+        .expect(200)
+        .expect('Hello world!')
+        .end(done);
+});
+
+it('should return my user object', (done) => {
+    request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body).toInclude({
+                name: 'Teodosio',
+                age: 45
+            });
+        })
+        .end(done);
+});
