707300d8537efb092e562219bbb849ee216fd9b5
Jordi Raya Lapuente <raya.jordi@gmail.com>
2018-11-09 15:47:29 +0100

    Section 8.88: User model

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,12 +1,36 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
+        trim: true,
+        unique: true,
+        validate: {
+            // validator: (value) => {
+            //     validator.isEmail(value);
+            // }, 
+            validator: validator.isEmail, // only need to define the function
+            message: '{VALUE} is not a valid email'
+        }       
     }, 
+    password: {
+        type: String,
+        required: true,
+        minlength: 6        
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 }); 
 
 module.exports = {User};
--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -88,6 +88,15 @@ app.patch('/todos/:id', (req, res) => {
     });   
 });
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+    user.save().then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
